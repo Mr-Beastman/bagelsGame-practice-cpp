03a51f86293dd703b7df6b786fe85cbb9be9eade
Mr-Beastman <Benjamin.d.eastman@gmail.com>
2024-05-08 20:10:15 +1200

    Add basic replay features
    DoWhile added around gameplay, that while true will keep starting a new game after the previous one has finihsed.
    
    Bool Function replayGame() added to prompt user to input y/n in regards to playing another game. This returns appropriate value to back to main to be picked up by the DoWhile loop.

--- bagelsGame/bagelsGame.cpp
+++ bagelsGame/bagelsGame.cpp
@@ -18,13 +18,13 @@ void generateHints(std::string userGuess, std::string randomNum) {
     int clueCount=0;
 
     for (int i = 0; i < 3; i++) {
-        //Loop through looking for matching numbers and placment. 
+        //Loop through checking if number and placment match. 
         if (userGuess[i] == randomNum[i]) {
             cout << "Pico ";
             clueCount++;
         }
         else {
-            //Loop through to number exists in sequence.
+            //Loop through to checking if number from guess appears in sequence at all.
             for (int j = 0; j < 3; j++) {
                 if (userGuess[i] == randomNum[j]) {
                     cout << "Fermi ";
@@ -34,15 +34,32 @@ void generateHints(std::string userGuess, std::string randomNum) {
         }
     }
 
+    //Quick check if any clues were provided and supplies final clue if needed.
     if (clueCount == 0) {
         cout << "Bagels";
     }
 }
 
+bool replayGame() {
+    char response;
+
+    cout << "\n\nWould you like to play again y/n? : ";
+    cin >> response;
+
+    if (response == 'y') {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
 int main() {
+    //Declare variables
     std::string userGuess, randomNum;
-    bool winCondition = 0;
+    bool replay;
     
+    //Game info Display
     cout << "Bagels, a deductive logic game.\n";
     cout << "\nI am thinking of a 3 digit number. Try to guess what it is.\n";
     cout << "Here are some clues:\n";
@@ -52,27 +69,33 @@ int main() {
     cout << std::left << std::setw(20) << "Fermi" << std::left << "One digit is in the correct position\n";
     cout << std::left << std::setw(20) << "Bagels" << std::left << "No digit is correct\n";
 
-    randomNum = randomNumGen();
+    do {
+        //Setting Random Number via function
+        randomNum = randomNumGen();
 
-    cout << "\nI have thought of a number.\n";
-    cout << "You have 10 guesses to get it\n";
-    cout << randomNum<<"\n";
+        cout << "\nI have thought of a number.\n";
+        cout << "You have 10 guesses to get it\n";
+        cout << randomNum<<"\n";
 
-    for (int guessCount = 1; guessCount <= 10; guessCount++) {
-        
-        cout << "Guess #" << guessCount<<"\n";
-        cin >> userGuess;
+        for (int guessCount = 1; guessCount <= 10; guessCount++) {
 
-        if (userGuess == randomNum) {
-            cout << "You win!";
-            break;
+            cout << "Guess #" << guessCount << "\n";
+            cin >> userGuess;
+
+            if (userGuess == randomNum) {
+                cout << "You win!";
+                break;
+            }
+            else {
+                generateHints(userGuess, randomNum);
+            }
+
+            cout << "\n";
         }
-        else {
-            generateHints(userGuess, randomNum);
-        }
 
-        cout << "\n";
-    }
+        replay = replayGame();
+
+    } while (replay == 1);
     
     cout << "\n";
     return 0;
